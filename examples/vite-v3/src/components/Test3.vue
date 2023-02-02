fb876bfd3a632b8e67ca41beb7eb46ccf29f615c
wyl <1052063507@qq.com>
2023-02-02 17:07:53 +0800

    feat: 更新依赖

--- examples/vite-v3/src/components/Test3.vue
+++ examples/vite-v3/src/components/Test3.vue
@@ -6,7 +6,15 @@
 
 <script setup inheritAttrs name="Test3">
 const a = 10
-debugger
 const b = 20
 const c = 30
+
+function fn1() {
+  let t = 10
+  return () => {
+    console.log(t)
+    debugger
+  }
+}
+fn1()()
 </script>
